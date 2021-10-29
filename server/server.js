afae01093a60a4535ae63c8d9350701e04ad44fb
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-29 15:44:19 -0400

    integrated client and server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -16,8 +18,8 @@ const app = express();
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
-    typeDefs, 
+  const server = new ApolloServer({
+    typeDefs,
     resolvers,
     context: authMiddleware
   });
@@ -37,6 +39,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
