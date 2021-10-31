0171355f554838d9d747d2560d5da77993f9eb8a
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-30 20:15:32 -0400

    adding graphql to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
+
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
